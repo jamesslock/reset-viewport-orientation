c3fdbeea61a6563c716f3c7a0f082fd3a2fda28d
James Seymour-Lock <im@james.sl>
2013-08-09 14:46:38 +0100

    reset javascript

---
+++ reset-viewport.js
@@ -0,0 +1,22 @@
+// trigger a reflow after orientation change to fix iphone bug
+var forceReflow = function(elem) {
+    elem = elem || document.documentElement;
+
+    // force a reflow by increasing size 1px
+    var width = elem.style.width,
+        px = elem.offsetWidth+1;
+
+    elem.style.width = px+'px';
+
+    setTimeout(function(){
+        // undo resize, unfortunately forces another reflow
+        elem.style.width = width;
+        elem = null;
+    }, 0);
+};
+
+window.addEventListener('orientationchange', function() {
+  setTimeout(function() {
+    forceReflow();
+  }, 50);
+});
